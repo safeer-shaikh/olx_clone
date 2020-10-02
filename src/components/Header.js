dd9f3e305dd7fc2a6fa7fb391dbe0b1ffe4add44
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-10-02 17:22:26 +0500

    added locations searchBar and Items searchBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,25 @@ class Header extends React.Component{
     render(){
         return(
             <div className="header_settings">
-                <img src={olxLogo} width="48" height="48"/>
+                <div className="image_settings">
+                    <img src={olxLogo} alt="olx Logo"/>
+                </div>
+                <div className="loc_search">
+                    <i className="fa fa-search"></i>
+                    <input className="loc_searchBar" type="text" placeholder="Pakistan"/>
+                    <button className="down_icon"><i class="fa fa-chevron-down"></i></button>
+                </div>
+                <div className="things_search">
+                    <input type="text" placeholder="  Find Cars, Mobile Phones and more..." />
+                    <div className="search_icon"><i class="fa fa-search"></i></div>
+                </div>
+                <div className="signinBtn">
+                    <a>Sign In</a>
+                    <button className="plus_icon">
+                        <div><i class="fa fa-plus"></i></div>
+                        <h2>Sell</h2>
+                    </button>
+                </div>
             </div>
         )
     }
