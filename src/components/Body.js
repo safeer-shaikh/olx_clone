f76e8f3e11720d23ccc9e5ac3e5c56ab3b3992ae
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-10-16 00:37:24 +0500

    added pictures and data in carousel

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,10 @@
 // import { render } from '@testing-library/react'
 import React from 'react'
 import '../App.css'
+import YourCard from './YourCard'
 import Items from './Items'
 
 class Body extends React.Component{
-    constructor(){
-        super()
-        this.state= {
-            count: 0,
-        }
-    }
     render(){   
         const liItems = [];
         const images = [require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),];
@@ -19,27 +14,6 @@ class Body extends React.Component{
         for (var i = 0; i < 5; i++){
             liItems[i] = <Items imageSource={images[i]}  price={price[i]} date={date[i]} data={data[i]}/>
         }
-        // function loadMore(){
-        //     console.log('yes working')
-        //     let count = 0;
-        //     const liItems = [];
-        //     const images = [require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),require('../images/phone1.jpg'),];
-        //     const price = ["16,500","16,600","16,700","16,800","16,900"];
-        //     const date = ["15 Sep","16 Sep","17 Sep","18 Sep","19 Sep"];
-        //     const data = ["data1","data2","data3","data4","data5"];
-        //     for (var i = 0; i < 5; i++){
-        //         liItems[i] = <Items imageSource={images[i]}  price={price[i]} date={date[i]} data={data[i]}/>
-        //         return(
-        //             <>
-        //             {
-        //                 liItems.map(liItems => 
-        //                     <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15,}}>{liItems}</li>
-        //                 )
-        //             }
-        //             </>
-        //         )
-        //     }
-        // }
         return(
             <div className="container-tm">
                 <div className="mainBody1">
@@ -51,22 +25,22 @@ class Body extends React.Component{
                         <div id="carouselExampleControls" class="carousel slide" data-interval="false">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <div className="fullCard "><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Branded Mobiles with best Quality"/></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
+                                    <div className="fullCard "><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,999" date="Sep 15" data="Branded Mobiles with best Quality"/></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone2.jpg')} price="Rs 13,999" date="Sep 18" data="Huawei mate 10 lite 4GB RAM" /></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone3.jpg')} price="Rs 18,999" date="3 Days Ago" data="moto one macro box packed" /></div>
+                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone4.jpg')} price="Rs 10,500" date="Sep 17" data="Huawei 6x 6gbrom 64gb ram" /></div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Branded Mobiles with best Quality"/></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone5.jpg')} price="Rs 3,600" date="Oct 06" data="Vivo Y51 mobile on isntallment"/></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone6.jpg')} price="Rs 15,999" date="3 days ago" data="Honor 9 4gb 64gb" /></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone7.jpg')} price="Rs 49,999" date="Today" data="S9 samsung 6/64 full box" /></div>
+                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone8.jpg')} price="Rs 73,000" date="Today" data="Iphone 7plus black" /></div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Branded Mobiles with best Quality"/></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard"><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
-                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone1.jpg')} price="Rs 16,499" date="15 Sep" data="Mobile Phone" /></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone9.jpg')} price="Rs 18,800" date="Oct 02" data="OPPO series"/></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone10.jpg')} price="Rs 6,500" date="Today" data="Qmobile i9 pro" /></div>
+                                    <div className="fullCard"><Items imageSource={require('../images/phone11.jpg')} price="Rs 9,000" date="Today" data="Iphone 6 16gb gold color" /></div>
+                                    <div className="fullCard" style={{marginRight: 15}}><Items imageSource={require('../images/phone12.jpg')} price="Rs 158,500" date="7 Days Ago" data="Iphone 11 pro max" /></div>
                                 </div>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -89,19 +63,21 @@ class Body extends React.Component{
                             {
                             liItems.map(liItems => 
                                 <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15,}}>{liItems}</li>
-                            )
-                            }
-                            <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15}}><Items /></li>
-                            {liItems.map(liItems => 
+                            )}
+                            <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15, backgroundColor: "blue",}}><YourCard /></li>
+                            {
+                            liItems.map(liItems => 
                                 <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15}}>{liItems}</li>
-                                )}
-                            {liItems.map(liItems => 
+                            )}
+                            {
+                            liItems.map(liItems => 
                                 <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15}}>{liItems}</li>
-                                )}
-                            {liItems.map(liItems => 
+                            )}
+                            {
+                            liItems.map(liItems => 
                                 <li style={{display: "inline-block",marginLeft: 15 , width: 285, marginTop: 15}}>{liItems}</li>
-                                )}
-                            <li><button>LOAD MORE</button></li>
+                            )}
+                            <li className="loadMoreLi"><button className="loadMoreBtn">Load More</button></li>
                         </ul>
                     </div>
                 </div>
---
+++ src/components/YourCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+class YourCard extends React.Component{
+    render(){
+        return(
+            <>
+                
+            </>
+        )
+    }
+}
+export default YourCard;
