962c8970b07e0885665712ba2f758d38a91141b0
safeer-shaikh <safeersohail210.ss@gmail.com>
2020-10-19 17:14:48 +0500

    added SubHeader in Products page

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import '../App.css'
+import './Products.css'
 import Header from '../components/Header'
 import SubHeader from '../components/SubHeader'
+import { nodeName } from 'jquery';
 
 class Products extends React.Component{
     render(){
+        let liData = ["iphone x","laptop","iphone 7plus","honda 125","honda city","honda civic","mehran","toyota corolla","cultus","mobile"];
         return(
             <div>
                 <Header />
                 <SubHeader />
+                <div>
+                    <div className="container">
+                        <div className="productsSubHeader">
+                            <ul className="products-subHeader-ul" >
+                                <li style={{fontWeight: 600, listStyleType: "none", fontSize: 12, padding: 6}}>Popular Searches:</li>
+                                {
+                                    liData.map((liData) => 
+                                    <span className="span-li-map">
+                                        <li><a href="#">{liData}</a></li><span>-</span>
+                                    </span>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
